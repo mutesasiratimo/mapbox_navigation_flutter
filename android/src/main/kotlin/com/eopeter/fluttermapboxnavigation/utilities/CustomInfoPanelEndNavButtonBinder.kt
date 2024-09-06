3cb9d2c48245ff8cfca859e4fe03986ac0bf92e8
Mateen Chaudhary <51848697+mateench38@users.noreply.github.com>
2024-09-06 19:00:51 +0500

    Update CustomInfoPanelEndNavButtonBinder.kt

--- android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/utilities/CustomInfoPanelEndNavButtonBinder.kt
+++ android/src/main/kotlin/com/eopeter/fluttermapboxnavigation/utilities/CustomInfoPanelEndNavButtonBinder.kt
@@ -12,31 +12,59 @@ import com.mapbox.navigation.ui.base.lifecycle.UIBinder
 import com.mapbox.navigation.ui.base.lifecycle.UIComponent
 import com.mapbox.navigation.ui.base.view.MapboxExtendableButton
 
+// package com.eopeter.fluttermapboxnavigation.utilities
+
+// import android.app.Activity
+// import android.view.ViewGroup
+// import com.eopeter.fluttermapboxnavigation.models.MapBoxEvents
+// import com.mapbox.navigation.core.MapboxNavigation
+// import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
+// import com.mapbox.navigation.ui.base.lifecycle.UIBinder
+// import com.mapbox.navigation.ui.base.lifecycle.UIComponent
+
 class CustomInfoPanelEndNavButtonBinder(
     val activity: Activity
 ) : UIBinder {
     override fun bind(viewGroup: ViewGroup): MapboxNavigationObserver {
-        val button = MapboxExtendableButton(
-            viewGroup.context,
-            null,
-            R.style.DropInStyleExitButton
-        )
-        button.iconImage.setImageResource(R.drawable.mapbox_ic_stop_navigation)
+        // Remove any existing views to ensure nothing is displayed
         viewGroup.removeAllViews()
-        viewGroup.addView(button)
-        button.updateMargins(
-            right = button.resources.getDimensionPixelSize(R.dimen.mapbox_infoPanel_paddingEnd)
-        )
 
+        // You can return an empty UIComponent to do nothing
         return object : UIComponent() {
             override fun onAttached(mapboxNavigation: MapboxNavigation) {
                 super.onAttached(mapboxNavigation)
-                button.setOnClickListener {
-                    mapboxNavigation.stopTripSession()
-                    PluginUtilities.sendEvent(MapBoxEvents.NAVIGATION_CANCELLED)
-                    //activity.finish()
-                }
+                // No action required since the button is not added
             }
         }
     }
 }
+
+
+// class CustomInfoPanelEndNavButtonBinder(
+//     val activity: Activity
+// ) : UIBinder {
+//     override fun bind(viewGroup: ViewGroup): MapboxNavigationObserver {
+//         val button = MapboxExtendableButton(
+//             viewGroup.context,
+//             null,
+//             R.style.DropInStyleExitButton
+//         )
+//         button.iconImage.setImageResource(R.drawable.mapbox_ic_stop_navigation)
+//         viewGroup.removeAllViews()
+//         viewGroup.addView(button)
+//         button.updateMargins(
+//             right = button.resources.getDimensionPixelSize(R.dimen.mapbox_infoPanel_paddingEnd)
+//         )
+
+//         return object : UIComponent() {
+//             override fun onAttached(mapboxNavigation: MapboxNavigation) {
+//                 super.onAttached(mapboxNavigation)
+//                 button.setOnClickListener {
+//                     mapboxNavigation.stopTripSession()
+//                     PluginUtilities.sendEvent(MapBoxEvents.NAVIGATION_CANCELLED)
+//                     //activity.finish()
+//                 }
+//             }
+//         }
+//     }
+// }
